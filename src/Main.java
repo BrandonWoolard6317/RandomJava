82163838f32f39b8853c9b1e9d3f34cfe69c2199
Unknown <brandon.woolard.2019@student.lhschools.org>
2018-12-20 02:13:11 -0500

    Created Java Class(x2)

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[]args){
+
+    }
+}
+
+class ClassA {
+    public static void Wow(){
+
+    }
+}
+
+class ClassB {
+    public static void Zar(){
+
+    }
+}
