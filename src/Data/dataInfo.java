dd067f7156f51411645637b61d2aa3e6557aed89
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-12-20 10:54:37 -0500

    Fixed methods

--- src/Data/dataInfo.java
+++ src/Data/dataInfo.java
@@ -6,21 +6,20 @@ public class dataInfo {
         System.out.println("Where do you want to drop?");
         System.out.println("1: Titled Towers\n2: Paradise Palms\n3: Polar Peak");
         r = usersInput.nextInt();
-        return "";
-    }
-    public static String calculateChestSpots(int r){
+        System.out.println("---------------------------------");
         if(r == 1){
-            System.out.println("|Total Possible Chest Spawns: 27|");
-            System.out.println("|Popularity: 2                  |");
+            System.out.println("|Total Possible Chest Spawns: 40|");
+            System.out.println("|Popularity: 3                  |");
         }
         else if(r == 2){
-            System.out.println("Total Possible Chest Spawns: 21");
-            System.out.println("Popularity: 4");
+            System.out.println("|Total Possible Chest Spawns: 22|");
+            System.out.println("|Popularity: 5                  |");
         }
         else if(r == 3){
-            System.out.println("Total Possible Chest Spawns: 31");
-            System.out.println("Popularity: 1");
+            System.out.println("|Total Possible Chest Spawns: ?|");
+            System.out.println("|Popularity: 6                  |");
         }
+        System.out.println("---------------------------------");
         return "";
     }
 }
